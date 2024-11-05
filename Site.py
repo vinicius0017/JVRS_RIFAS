6fffe9987f8b1d2bae9251962e0e9ff2dd3bd9a8
vinicius guedes <guedesvinicius258@gmail.com>
2024-11-05 14:37:29 -0300

    jvrc_rifas

--- Site.py
+++ Site.py
@@ -6,7 +6,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def pagina_inicial():
-    return render_template('pagina_inicial.html')
+    return render_template('index.html')
 
 @app.route("/contatos")
 def contatos():
